7d98ad505c593f2d10127d4b6072d51f0844f751
Mark Triggs <mark@dishevelled.net>
2012-08-29 10:06:45 +1000

    Clean up all indentation and whitespace

--- common/spec/jsonmodel_spec.rb
+++ common/spec/jsonmodel_spec.rb
@@ -30,7 +30,7 @@ end
 
 
 class StubHTTP
-  def request (req)
+  def request(req)
     StubResponse.new
   end
   def code
@@ -48,9 +48,9 @@ end
 
 describe JSONModel do
   before(:each) do
-    
+
     BACKEND_SERVICE_URL = 'http://example.com'
-    
+
     schema = '{
       :schema => {
         "$schema" => "http://www.archivesspace.org/archivesspace.json",
@@ -82,53 +82,53 @@ describe JSONModel do
     File.stub(:basename){ 'stub' }
     File.stub_chain("open.read") { schema }
     # File.stub(:open).with('stub'){'stub'}
-    
+
 
     Net::HTTP.stub(:start){ StubHTTP.new }
-    
+
     JSONModel::init( { :client_mode => true, :url => "http://example.com", :strict_mode => true } )
-    
+
     @klass = Klass.new
   end
-  
+
   it "should supply a class for a loaded schema" do
-    
+
     @klass.JSONModel(:stub).to_s.should eq('JSONModel(:stub)')
-    
+
   end
-  
+
   it "should create an instance when given a hash" do
-  
+
     jo = @klass.JSONModel(:stub).from_hash({"ref_id" => "abc", "title"=> "Stub Object"})
     jo.ref_id.should eq("abc")
-  
+
   end
-  
+
   it "should be able to determine the uri for a class instance give an id and a repo_id" do
-  
+
     @klass.JSONModel(:stub).uri_for(500, :repo_id => "1").should eq('/repositories/1/stubs/500')
-  
+
   end
-  
+
   it "should be able to save an instance of a model" do
     jo = @klass.JSONModel(:stub).from_hash({:ref_id => "abc", :title=> "Stub Object"})
     jo.save()
     jo.to_hash.has_key?('uri').should be_true
   end
-  
+
   it "should create an instance when given a hash using symbols for keys" do
-  
+
     jo = @klass.JSONModel(:stub).from_hash({:ref_id => "abc", :title=> "Stub Object"})
     jo.ref_id.should eq("abc")
-  
+
   end
-  
+
   it "should have its repo id in its uri after being saved" do
     jo = @klass.JSONModel(:stub).from_hash({:ref_id => "abc", :title=> "Stub Object"})
     jo.save("repo_id" => 2)
     jo.uri.should eq('/repositories/2/stubs/999')
   end
-  
-  
+
+
 
 end
