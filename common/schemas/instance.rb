3b0c49ed5b24b158cdc3255b232b67c45e81511e
Payten Giles <payten.giles@gmail.com>
2012-10-08 18:26:15 +1100

    delete all sub records when updating a JSONModel object; fix show templates for instance/container;

--- common/schemas/instance.rb
+++ common/schemas/instance.rb
@@ -5,7 +5,7 @@
     "properties" => {
       "instance_type" => {"type" => "string", "minLength" => 1, "required" => true, "enum" => ["audio", "books", "computer_disks", "digital_object", "digital_object_link", "graphic_materials", "maps", "microform", "mixed_materials", "moving_images", "realia", "text"]},
 
-      "container" => {"type" => "object", "type" => "JSONModel(:container) object"},
+      "container" => {"type" => "JSONModel(:container) object"},
     },
 
     "additionalProperties" => false,
