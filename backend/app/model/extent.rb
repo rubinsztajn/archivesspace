dc6fd6d9fb1ecf01c8eb21da3ba7a5bdfd7d69e7
Brian Hoffman <brianjhoffman@gmail.com>
2012-11-06 14:54:01 -0500

    force extent to link to most granular record type

--- backend/app/model/extent.rb
+++ backend/app/model/extent.rb
@@ -3,4 +3,15 @@ class Extent < Sequel::Model(:extent)
   set_model_scope :repository
 
   plugin :validation_helpers
+  
+  def self.create_from_json(json, opts = {})
+
+    # Assume the Extent statement applies to the more granular
+    # thing.
+    if opts["resource_id"] and opts[:archival_object_id]
+      opts.delete("resource_id")
+    end
+    
+    super
+  end
 end
