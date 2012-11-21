f9421e520b0479ea0836f71ccf070863c37b32fe
jambun <jambun@gmail.com>
2012-11-21 11:28:16 +1100

    added validation for linked records

--- backend/spec/model_collection_management_spec.rb
+++ backend/spec/model_collection_management_spec.rb
@@ -12,7 +12,42 @@ describe 'Collection Management model' do
   it "enforces one linked accession or resource, or one or more digital objects" do
 
     expect {
+      create(:json_collection_management,
+             :linked_records => [{'ref' => create(:json_accession).uri}])
+    }.to_not raise_error(JSONModel::ValidationException)
+
+    expect {
+      create(:json_collection_management,
+             :linked_records => [{'ref' => create(:json_resource).uri}])
+    }.to_not raise_error(JSONModel::ValidationException)
+
+    expect {
+      create(:json_collection_management,
+             :linked_records => [{'ref' => create(:json_digital_object).uri},
+                                 {'ref' => create(:json_digital_object).uri},
+                                 {'ref' => create(:json_digital_object).uri}])
+    }.to_not raise_error(JSONModel::ValidationException)
+
+    expect {
       create(:json_collection_management, :linked_records => [])
+    }.to raise_error(JSONModel::ValidationException)
+
+    expect {
+      create(:json_collection_management,
+             :linked_records => [{'ref' => create(:json_accession).uri},
+                                 {'ref' => create(:json_accession).uri}])
+    }.to raise_error(JSONModel::ValidationException)
+
+    expect {
+      create(:json_collection_management,
+             :linked_records => [{'ref' => create(:json_resource).uri},
+                                 {'ref' => create(:json_resource).uri}])
+    }.to raise_error(JSONModel::ValidationException)
+
+    expect {
+      create(:json_collection_management,
+             :linked_records => [{'ref' => create(:json_accession).uri},
+                                 {'ref' => create(:json_digital_object).uri}])
     }.to raise_error(JSONModel::ValidationException)
 
   end
